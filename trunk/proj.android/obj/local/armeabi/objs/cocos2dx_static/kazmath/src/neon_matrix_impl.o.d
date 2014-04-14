77fe2edb8b993bb6bd57828c889ae1f75726f697
longpham.uit@gmail.com <longpham.uit@gmail.com@e7e494bb-703a-9d0d-0d7f-f3b7cde52762>
2014-04-14 09:25:47 +0000

    ignore

--- trunk/proj.android/obj/local/armeabi/objs/cocos2dx_static/kazmath/src/neon_matrix_impl.o.d
+++
@@ -1,5 +0,0 @@
-obj/local/armeabi/objs/cocos2dx_static/kazmath/src/neon_matrix_impl.o: \
- /cygdrive/e/Cocos2DX2/projects/PhiCongButChi/proj.android/../../../cocos2dx/kazmath/src/neon_matrix_impl.c \
- /cygdrive/e/Cocos2DX2/projects/PhiCongButChi/proj.android/../../../cocos2dx/kazmath/include/kazmath/neon_matrix_impl.h
-
-/cygdrive/e/Cocos2DX2/projects/PhiCongButChi/proj.android/../../../cocos2dx/kazmath/include/kazmath/neon_matrix_impl.h:
