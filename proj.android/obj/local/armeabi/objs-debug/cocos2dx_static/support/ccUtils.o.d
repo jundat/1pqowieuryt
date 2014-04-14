bb37f73e6952fbe37bc792ef9e03467dc21daa56
longpham.uit <longpham.uit@gmail.com>
2014-04-14 09:25:47 +0000

    ignore

--- proj.android/obj/local/armeabi/objs-debug/cocos2dx_static/support/ccUtils.o.d
+++
@@ -1,5 +0,0 @@
-obj/local/armeabi/objs-debug/cocos2dx_static/support/ccUtils.o: \
- /cygdrive/e/Cocos2DX2/projects/PhiCongButChi/proj.android/../../../cocos2dx/support/ccUtils.cpp \
- /cygdrive/e/Cocos2DX2/projects/PhiCongButChi/proj.android/../../../cocos2dx/support/ccUtils.h
-
-/cygdrive/e/Cocos2DX2/projects/PhiCongButChi/proj.android/../../../cocos2dx/support/ccUtils.h:
--- proj.android/obj/local/armeabi/objs/cocos2dx_static/ccFPSImages.o.d
+++
@@ -1,5 +0,0 @@
-obj/local/armeabi/objs/cocos2dx_static/ccFPSImages.o: \
- /cygdrive/e/Cocos2DX2/projects/PhiCongButChi/proj.android/../../../cocos2dx/ccFPSImages.c \
- /cygdrive/e/Cocos2DX2/projects/PhiCongButChi/proj.android/../../../cocos2dx/ccFPSImages.h
-
-/cygdrive/e/Cocos2DX2/projects/PhiCongButChi/proj.android/../../../cocos2dx/ccFPSImages.h:
