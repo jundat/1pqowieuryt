e3b0f744eef51fa44c257c6ed07f11075e2454f4
longpham.uit <longpham.uit@gmail.com>
2014-04-15 02:55:11 +0000

    *** Change to 2.1.5 OK ***

--- proj.ios/RootViewController.mm
+++ proj.ios/RootViewController.mm
@@ -74,42 +74,153 @@
 //------------------- JNI FUNCTION --------------------//
 
 
+//------------- RATE --------------//
 
 - (void) Rate:(NSObject *)prms
 {
-    NSLog(@"Rate called in Object-C");
     NSDictionary *parameters = (NSDictionary*)prms;
-    NSLog(@"Passed params are : %@", parameters);
+    //NSLog(@"Passed params are : %@", parameters);
     
-    //isSuccess
-    //true
-    //false
+    NSString *game_name, *game_package, *msg, *rate, *later, *no;
     
-    //responseType
-    //RATE
-    //REMIND_ME_LATER
-    //NO_THANKS
+    game_name = (NSString*)[parameters objectForKey:@"game_name"];
+    game_package = (NSString*)[parameters objectForKey:@"game_package"];
+    msg = (NSString*)[parameters objectForKey:@"msg"];
+    rate = (NSString*)[parameters objectForKey:@"rate"];
+    later = (NSString*)[parameters objectForKey:@"later"];
+    no = (NSString*)[parameters objectForKey:@"no"];
+    game_name = (NSString*)[parameters objectForKey:@"game_name"];
     
-    //String jsonStr = "{\"isSuccess\" : true, " +
-    //"\"responseType\": \"" + responseType + "\"" +
-    //"}";
+    [iRate sharedInstance].applicationBundleID = game_package;
+    [iRate sharedInstance].messageTitle = game_name;
+    [iRate sharedInstance].message = msg;
+    [iRate sharedInstance].rateButtonLabel = rate;
+    [iRate sharedInstance].remindButtonLabel = later;
+    [iRate sharedInstance].cancelButtonLabel = no;
     
-    /*
-     
+    [[iRate sharedInstance] promptForRating];
+}
+
+
+
+//no thanks
+- (void)iRateUserDidDeclineToRateApp
+{
+    NSLog(@"no thanks");
+    NSString *jsonStr = @"{\"isSuccess\": true, \"responseType\": \"NO_THANKS\"}";
+    NSData *jsonData = [jsonStr dataUsingEncoding:NSUTF8StringEncoding];
+    NSError *e = nil;
+    NSDictionary *dict = [NSJSONSerialization JSONObjectWithData:jsonData options:nil error:&e];
+    
+    [IOSNDKHelper SendMessage:@"onRateCompleted" WithParameters:dict];
+
+}
+
+//remind
+- (void)iRateUserDidRequestReminderToRateApp
+{
+    NSLog(@"remind");
+    NSString *jsonStr = @"{\"isSuccess\": true, \"responseType\": \"REMIND_ME_LATER\"}";
+    NSData *jsonData = [jsonStr dataUsingEncoding:NSUTF8StringEncoding];
+    NSError *e = nil;
+    NSDictionary *dict = [NSJSONSerialization JSONObjectWithData:jsonData options:nil error:&e];
+    
+    [IOSNDKHelper SendMessage:@"onRateCompleted" WithParameters:dict];
+}
+
+//after rate
+- (void)iRateDidOpenAppStore
+{
+    NSLog(@"rated");
     NSString *jsonStr = @"{\"isSuccess\": true, \"responseType\": \"RATE\"}";
     NSData *jsonData = [jsonStr dataUsingEncoding:NSUTF8StringEncoding];
     NSError *e = nil;
     NSDictionary *dict = [NSJSONSerialization JSONObjectWithData:jsonData options:nil error:&e];
     
     [IOSNDKHelper SendMessage:@"onRateCompleted" WithParameters:dict];
+}
+
+//------------- END RATE --------------//
+//-------- SHOW CHARGE WEB VIEW ----------//
+
+
+- (void) ShowChargeWebView:(NSObject *)prms
+{
     
-     */
+    NSDictionary *parameters = (NSDictionary*)prms;
+    NSLog(@"Passed params are : %@", parameters);
     
-    [[iRate sharedInstance] promptForRating]; //promptForRating //promptIfNetworkAvailable
+    NSString *charge_caption, *charge_close, *charge_link;
+    
+    charge_caption = (NSString*)[parameters objectForKey:@"charge_caption"];
+    charge_close = (NSString*)[parameters objectForKey:@"charge_close"];
+    charge_link = (NSString*)[parameters objectForKey:@"charge_link"];
+
+    CGRect screenRect = [[UIScreen mainScreen] bounds];
+    CGFloat screenWidth = screenRect.size.width;
+    CGFloat screenHeight = screenRect.size.height;
+    
+    UIWebView *webView = [[UIWebView alloc] initWithFrame:CGRectMake(0, 0, screenWidth, screenHeight)];
+    [self.view addSubview:webView];
+    
+    NSURLRequest *urlRequest;
+    NSURL *urlforWebView;
+    urlforWebView=[NSURL URLWithString:@"http://www.google.com"];
+    urlRequest=[NSURLRequest requestWithURL:urlforWebView];
+    [webView loadRequest:urlRequest];
+ 
+    
+    // A rounded Rect button created by using class method
+    UIButton *roundRectButton = [UIButton buttonWithType:
+                                 UIButtonTypeInfoDark];
+    [roundRectButton setFrame:CGRectMake(60, 50, 100, 40)];
+    // sets title for the button
+    [roundRectButton setTitle:@"Close" forState:
+     UIControlStateNormal];
+    
+    [self.view addSubview:roundRectButton];
+    
+    UIButton *customButton = [UIButton buttonWithType: UIButtonTypeCustom];
+    [customButton setBackgroundColor: [UIColor whiteColor]];
+    [customButton setTitleColor:[UIColor blackColor] forState:
+     UIControlStateHighlighted];
+    [customButton setTitleColor:[UIColor blackColor] forState:
+     UIControlStateNormal];
+    //sets background image for normal state
+    [customButton setBackgroundImage:[UIImage imageNamed:
+                                      @"button.png"]
+                            forState:UIControlStateNormal];
+    //sets background image for highlighted state
+    [customButton setBackgroundImage:[UIImage imageNamed:
+                                      @"button_down.png"]
+                            forState:UIControlStateHighlighted];
+    [customButton setFrame:CGRectMake(60, 100, 200, 40)];
+    [customButton setTitle:@"Close" forState:UIControlStateNormal];
+    [self.view addSubview:customButton];
+    
+    
+    
+    
+    /*
+    UIAlertView *av = [[UIAlertView alloc] initWithTitle:@"HUONG DAN" message:nil delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];    [av setFrame:CGRectMake(0, 0, 320, 480)];
+    UIWebView *webView = [[UIWebView alloc] initWithFrame:CGRectMake(0, 0, 260, 440)];
+    webView.scalesPageToFit = YES;
+    [self.view addSubview:webView];
+    NSURLRequest *urlRequest;
+    NSURL *urlforWebView;
+    urlforWebView=[NSURL URLWithString:@"http://www.google.com"];
+    urlRequest=[NSURLRequest requestWithURL:urlforWebView];
+    [webView loadRequest:urlRequest];
+    [av setValue:webView forKey:@"accessoryView"];
+    [av show];
+    */
+    
 }
 
 
-//------------------- JNI FUNCTION --------------------//
+
+
+
 
 
 @end
