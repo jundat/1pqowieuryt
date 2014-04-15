44f119ff74d40a2791f071d5bb4f82285521f552
longpham.uit@gmail.com <longpham.uit@gmail.com@e7e494bb-703a-9d0d-0d7f-f3b7cde52762>
2014-04-15 03:19:56 +0000

    + Hide status bar in iOS 7

--- trunk/proj.ios/RootViewController.h
+++ trunk/proj.ios/RootViewController.h
@@ -8,23 +8,25 @@
 
 
 //------------- rate ---------------//
-
 - (void) Rate:(NSObject *)prms;
-
 //no thanks
 - (void)iRateUserDidDeclineToRateApp;
-
 //remind
 - (void)iRateUserDidRequestReminderToRateApp;
-
 //after rate
 - (void)iRateDidOpenAppStore;
 
-//----------- end rate -------------//
+
 //------ show charge webview --------//
-
-
 - (void) ShowChargeWebView:(NSObject *)prms;
+
+
+//------ status bar in iOS 7 --------//
+- (void)viewDidLoad;
+- (BOOL)prefersStatusBarHidden;
+
+
+
 
 
 @end
