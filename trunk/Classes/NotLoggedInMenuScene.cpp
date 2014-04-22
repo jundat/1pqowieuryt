5ea99058dfa1bb6c97d191ed234ab943044e8c1c
longpham.uit@gmail.com <longpham.uit@gmail.com@e7e494bb-703a-9d0d-0d7f-f3b7cde52762>
2014-04-22 10:30:54 +0000

    + new resource ok: NotLoggedInMenuScene

--- trunk/Classes/NotLoggedInMenuScene.cpp
+++ trunk/Classes/NotLoggedInMenuScene.cpp
@@ -5,8 +5,9 @@
 #include "DataManager.h"
 #include "QuitDialog.h"
 #include "MenuScene.h"
-#include <time.h>
 #include "TextLoader.h"
+#include <time.h>
+
 
 USING_NS_CC;
 USING_NS_CC_EXT;
@@ -26,59 +27,73 @@ bool NotLoggedInMenuScene::init()
     {
         return false;
     }
-
-	this->setKeypadEnabled(true);
 	
-	string lang = DataManager::sharedDataManager()->GetLanguage();
-	if (lang.compare("English") == 0)
-	{
-		m_bg = CCSprite::create("not_logged_bg.png");
-	}
-	else
-	{
-		m_bg = CCSprite::create("not_logged_bg.png");
-	}	
-	
+    //
+    //bg
+    //
+    m_bg = CCSprite::create("not_logged_bg.png");
 	m_bg->setAnchorPoint(CCPointZero);
 	m_bg->setPosition(CCPointZero);
 	this->addChild(m_bg, 0);
-	   
-    string en_start_normal = string("en_new_button.png");
-    string en_start_press = string("en_new_button_press.png");
     
-    string vn_start_normal = string("vn_new_button.png");
-    string vn_start_press = string("vn_new_button_press.png");
+    float SCALE_BUTTON = 1.163f;
+    //
+    //log in
+    //
+    m_facebookItem = CCMenuItemImage::create(
+         "button.png",
+         "button_down.png",
+         this,
+         menu_selector(NotLoggedInMenuScene::facebookCallback));
+    m_facebookItem->setScale(SCALE_BUTTON);
+    m_facebookItem->setPosition(ccp(214, 1280-662));
     
-	//
-	if (lang.compare("English") == 0)
-	{
-		m_playItem = CCMenuItemImage::create(
-			"en_new_button.png",
-			"en_new_button_press.png",
-			this,
-			menu_selector(NotLoggedInMenuScene::playCallback));
-	} 
-	else
-	{
-		m_playItem = CCMenuItemImage::create(
-			"vn_new_button.png",
-			"vn_new_button_press.png",
-			this,
-			menu_selector(NotLoggedInMenuScene::playCallback));
-	}
+    //
+    //play test
+    //
+    m_playItem = CCMenuItemImage::create(
+         "button.png",
+         "button_down.png",
+         this,
+         menu_selector(NotLoggedInMenuScene::playCallback));
+    m_playItem->setScale(SCALE_BUTTON);
+    m_playItem->setPosition(ccp(589, 1280-662));
+    
 
-	m_playItem->setPosition(ccp(400, 1280-813));
-    
-    
-    //Log in facebook
-    CCMenuItemImage* itFacebook = CCMenuItemImage::create("button.png", "button_down.png", "button_down.png", this, menu_selector(NotLoggedInMenuScene::facebookCallback));
-    itFacebook->setPosition(ccp(400, 2*itFacebook->getContentSize().height));
-
-    m_menu = CCMenu::create(m_playItem, itFacebook, NULL);
+    m_menu = CCMenu::create(m_playItem, m_facebookItem, NULL);
     m_menu->setPosition(CCPointZero);
-    this->addChild(m_menu, 1);
-
+    this->addChild(m_menu);
+    
+    CCPoint ANCHOR_TEXT = ccp(0.4f, 0.3f);
+    //
+    //text
+    //
+    CCLabelTTF* txtLogIn = CCLabelTTF::create(TXT("try_login"), G_FONT_NORMAL, 48);
+    txtLogIn->setColor(ccBLACK);
+    txtLogIn->setAnchorPoint(ANCHOR_TEXT);
+    txtLogIn->setPosition(m_facebookItem->getPosition());
+    this->addChild(txtLogIn);
+    
+    CCLabelTTF* txtPlay = CCLabelTTF::create(TXT("try_play_test"), G_FONT_NORMAL, 48);
+    txtPlay->setColor(ccBLACK);
+    txtPlay->setAnchorPoint(ANCHOR_TEXT);
+    txtPlay->setPosition(m_playItem->getPosition());
+    this->addChild(txtPlay);
+    
+    //
+    //image
+    //
+    CCSprite* sprFb = CCSprite::create("fb_button.png");
+    sprFb->setPosition(ccp(91, 1280-651));
+    this->addChild(sprFb);
+    
+    CCSprite* sprPlay = CCSprite::create("fire_button.png");
+    sprPlay->setPosition(ccp(474, 1280-654));
+    this->addChild(sprPlay);
+    
+    
 	
+	this->setKeypadEnabled(true);
 	STOP_BACKGROUND_MUSIC;
     return true;
 }
